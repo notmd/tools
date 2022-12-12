52f7907eeb9032385469b78c9fa01e529a86aab5
notmd <tinhmeo10@gmail.com>
2022-12-13 00:26:10 +0700

    wip

---
+++ crates/rome_js_analyze/tests/specs/nursery/useHeadingContent/invalid.jsx
@@ -0,0 +1,25 @@
+let a = <h1 />;
+let a = (
+	<h1>
+		<Bar aria-hidden />
+	</h1>
+);
+let a = <h1>{undefined}</h1>;
+let a = (
+	<h1>
+		<input type="hidden" />
+	</h1>
+);
+let a = <Heading />;
+let a = (
+	<Heading>
+		<Bar aria-hidden />
+	</Heading>
+);
+let a = <Heading>{undefined}</Heading>;
+let a = <Heading />;
+let a = (
+	<h1>
+		<CustomInput type="hidden" />
+	</h1>
+);
---
+++ crates/rome_js_analyze/tests/specs/nursery/useHeadingContent/valid.jsx
@@ -0,0 +1,35 @@
+let a = <div />;
+let a = <h1>Foo</h1>;
+let a = <h2>Foo</h2>;
+let a = <h3>Foo</h3>;
+let a = <h4>Foo</h4>;
+let a = <h5>Foo</h5>;
+let a = <h6>Foo</h6>;
+let a = <h6>123</h6>;
+let a = (
+	<h1>
+		<Bar />
+	</h1>
+);
+let a = <h1>{foo}</h1>;
+let a = <h1>{foo.bar}</h1>;
+let a = <h1 dangerouslySetInnerHTML={{ __html: "foo" }} />;
+let a = <h1 children={children} />;
+let a = <Heading>Foo</Heading>;
+let a = <Title>Foo</Title>;
+let a = (
+	<Heading>
+		<Bar />
+	</Heading>
+);
+let a = <Heading>{foo}</Heading>;
+let a = <Heading>{foo.bar}</Heading>;
+let a = <Heading dangerouslySetInnerHTML={{ __html: "foo" }} />;
+let a = <Heading children={children} />;
+let a = <h1 aria-hidden />;
+let a = <Heading>Foo</Heading>;
+let a = (
+	<h1>
+		<CustomInput type="hidden" />
+	</h1>
+);
